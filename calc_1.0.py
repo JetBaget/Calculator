ee1f0fe1a805221713bfa0d6893e436f850727a1
Chromov Anton <jetneitrino@gmail.com>
2016-07-28 22:11:26 +0300

    First step

---
+++ calc_1.0.py
@@ -0,0 +1,9 @@
+#/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+'''This program allows you to calculate the sum of two integers'''
+
+num_1 = int(raw_input('Enter 1-st integer: '))
+num_2 = int(raw_input('Enter 2-nd integer: '))
+res = num_1 + num_2
+print 'The sum is equal:', res

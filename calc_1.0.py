0ecd1ce000b782257f0ead5f6c8a7732bce6df42
Chromov Anton <jetneitrino@gmail.com>
2016-07-29 14:52:10 +0300

    First step Remake#1

--- calc_1.0.py
+++ calc_1.0.py
@@ -1,9 +1,19 @@
 #/usr/bin/env python
 #! -*- coding: utf-8 -*-
+from sys import stdin
 
 '''This program allows you to calculate the sum of two integers'''
 
-num_1 = int(raw_input('Enter 1-st integer: '))
-num_2 = int(raw_input('Enter 2-nd integer: '))
-res = num_1 + num_2
-print 'The sum is equal:', res
+input_str = stdin.read()
+res = 0
+
+#OPERATION_1: Addition
+
+split_input_str = input_str.split()
+
+if split_input_str[0].isdigit() and split_input_str[1] == '+'\
+ and split_input_str[2].isdigit():
+    res = int(split_input_str[0]) + int(split_input_str[2])
+    print 'The sum is equal:', res
+else:
+    print 'The string is not correct'

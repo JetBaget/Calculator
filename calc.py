a6c6d0f4924aef2adb9e4067ce8bf351be921446
Chromov Anton <jetneitrino@gmail.com>
2016-07-31 23:09:37 +0300

    Ver. control, last changes date, +/-, long input

--- calc.py
+++ calc.py
@@ -1,18 +1,92 @@
 #/usr/bin/env python
 #! -*- coding: utf-8 -*-
-__version__ = "1.1"
-
+import argparse
+import time
+from os import path
 from sys import stdin
 
+#Initialisation of variables
 input_str = stdin.readline()
+processed_str = ''
+operators = []
+o_perands = []
+number = ''
 res = 0
-if ' ' in input_str:
-    input_str = input_str.replace(' ', '')
 
-#OPERATION_1: Addition
+#Version and changes date control
+ver = "1.2"
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--version", action="store_true")
+args = parser.parse_args()
+if args.version:
+    print ("Version: {}".format(ver))
+    date_of_create = path.getmtime("calc.py")
+    print ("The date of last changes: {}".format(time.ctime(date_of_create)))
+    exit(0)
 
-if input_str[0].isdigit() and '+' in input_str and input_str[2].isdigit():
-    res = int(input_str[0]) + int(input_str[2])
-    print res
+#Classes
+class InStr_proc():
+
+    def __init__(self, string):
+        self.string = string
+
+    def processor(self):
+        if ' ' in self.string:
+            self.string = self.string.replace(' ', '')
+            return self.string
+        else:
+            return self.string
+
+class Queue():
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+ 
+    def peek(self):
+        return self.items[0]
+
+#Input string processing
+processed_str = InStr_proc(input_str).processor()
+operators = Queue()
+o_perands = Queue()
+
+#Cheking the correctness of Input
+
+for i in processed_str:
+    if i in '+-':
+        operators.enqueue(i)
+        if number != '':
+            o_perands.enqueue(number)
+        number = ''
+    elif i.isdigit():
+        number += i
+    else:
+        o_perands.enqueue(number)
+
+#Calculating
+if operators.peek() == '-' and operators.size() == o_perands.size():
+    res -= int(o_perands.dequeue())
+    operators.dequeue()
 else:
-    print 'The string is not correct'
+    res += int(o_perands.dequeue())
+while o_perands.isEmpty() != True:
+    if operators.peek() == '+':
+        res += int(o_perands.dequeue())
+        operators.dequeue()
+    elif operators.peek() == '-':
+        res -= int(o_perands.dequeue())
+        operators.dequeue()
+
+print res
